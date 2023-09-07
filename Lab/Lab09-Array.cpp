bac8a725abac5e9bce3110c068491c295b71834c
RubyQWER <Chanatpong1234@gmail.com>
2023-09-07 15:19:54 +0700

    lap9

--- Lab/Lab09-Array.cpp
+++ Lab/Lab09-Array.cpp
@@ -18,14 +18,56 @@
             2
     Output:
         Index:  0  1  2  3  4  5
-        Array:  # 19  #  #  #  2
+        Array: 19  #  #  #  #  2
 */
+
+
 
 #include <stdio.h>
 
-int main() {
+// ฟังก์ชันเช็คว่าเป็นจำนวนเฉพาะหรือไม่
+int isPrime( int n ) {
+    if ( n <= 1 ) {
+        return 0 ; // ไม่ใช่จำนวนเฉพาะ
+    }
+    for ( int i = 2 ; i * i <= n ; i++ ) {
+        if ( n % i == 0 ) {
+            return 0 ; // ไม่ใช่จำนวนเฉพาะ
+        }
+    }
+    return 1 ; // เป็นจำนวนเฉพาะ
+}
 
-    //--| YOUR CODE HERE
+int main() {
+    int N ;
+    printf( "Enter N: " ) ;
+    scanf( "%d" , &N ) ;
+
+    int values[ N ] ;
+
+    // รับค่าจากผู้ใช้และตรวจสอบว่าเป็นจำนวนเฉพาะหรือไม่
+    for ( int i = 0 ; i < N ; i++ ) {
+        printf( " Enter value[%d] : " , i ) ;
+        scanf( "%d" , &values[i] ) ;
+        if ( !isPrime( values[i] ) ) {
+            values[i] = -1 ; // ใช้ -1 แทนค่าที่ไม่ใช่จำนวนเฉพาะ
+        }
+    }
+
+    // แสดงผลลัพธ์
+    printf( "Index:  " ) ;
+    for ( int i = 0 ; i < N ; i++ ) {
+        printf( " %2d  " , i ) ;
+    }
+    printf( " \nArray:  " ) ;
+    for ( int i = 0 ; i < N ; i++ ) {
+        if ( values[i] == -1 ) {
+            printf( " #  " ) ;
+        } else {
+            printf( " %2d  " , values[i] ) ;
+        }
+    }
+    printf( " \n " );
 
     return 0 ;
-}//end main function
+}
