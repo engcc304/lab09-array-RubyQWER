262cfdb5ba7184ecd09a7bfec0ba8921964e5a2c
RubyQWER <Chanatpong1234@gmail.com>
2023-09-07 15:23:29 +0700

    lap 9

--- Lab/Lab09-Array.cpp
+++ Lab/Lab09-Array.cpp
@@ -29,12 +29,12 @@
 int isPrime( int n ) {
     if ( n <= 1 ) {
         return 0 ; // ไม่ใช่จำนวนเฉพาะ
-    }
+    }//end if
     for ( int i = 2 ; i * i <= n ; i++ ) {
         if ( n % i == 0 ) {
             return 0 ; // ไม่ใช่จำนวนเฉพาะ
-        }
-    }
+        }//end if
+    }//end for
     return 1 ; // เป็นจำนวนเฉพาะ
 }
 
@@ -49,25 +49,29 @@ int main() {
     for ( int i = 0 ; i < N ; i++ ) {
         printf( " Enter value[%d] : " , i ) ;
         scanf( "%d" , &values[i] ) ;
+        
         if ( !isPrime( values[i] ) ) {
             values[i] = -1 ; // ใช้ -1 แทนค่าที่ไม่ใช่จำนวนเฉพาะ
-        }
-    }
+        }//end if
+    }//end for
 
     // แสดงผลลัพธ์
     printf( "Index:  " ) ;
+
     for ( int i = 0 ; i < N ; i++ ) {
         printf( " %2d  " , i ) ;
-    }
+    }//end for
+
     printf( " \nArray:  " ) ;
     for ( int i = 0 ; i < N ; i++ ) {
         if ( values[i] == -1 ) {
             printf( " #  " ) ;
-        } else {
+        }//end if
+         else {
             printf( " %2d  " , values[i] ) ;
-        }
-    }
-    printf( " \n " );
+        }//end else
+    }//end for
+    printf( " \n " ) ;
 
     return 0 ;
-}
+}//end function
